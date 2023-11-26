f0c4d7287717790e90569556268fc5ec054ff2d2
highlander <highlander9200@gmail.com>
2023-11-26 03:12:09 -0300

    Added lualine, mason and telescope

--- init.lua
+++ init.lua
@@ -2,5 +2,9 @@ require "highlander.spec"
 require "highlander.options"
 
 spec "highlander.colorscheme"
+spec "highlander.lualine"
+spec "highlander.mason"
+spec "highlander.lspconfig"
+spec "highlander.telescope"
 
 require "highlander.lazy"
