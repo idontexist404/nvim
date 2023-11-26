68c7a3fc8a1e2f2ef1d14693eff24def4e42b454
highlander <highlander9200@gmail.com>
2023-11-26 04:02:01 -0300

    Some good-to-have like indentline and autopairs, and some necessities like code completion and toggle comment

--- init.lua
+++ init.lua
@@ -1,10 +1,16 @@
 require "highlander.spec"
 require "highlander.options"
 
-spec "highlander.colorscheme"
-spec "highlander.lualine"
 spec "highlander.mason"
 spec "highlander.lspconfig"
+spec "highlander.cmp"
+
+spec "highlander.colorscheme"
+spec "highlander.lualine"
 spec "highlander.telescope"
+spec "highlander.autopairs"
+spec "highlander.comment"
+spec "highlander.indentline"
+spec "highlander.illuminate"
 
 require "highlander.lazy"
